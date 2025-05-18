d9af40228cff0bfbf23ad5ed84c8429607c92e3e
assignment <aliya_sadvokasova01@mail.ru>
2025-05-18 16:03:40 +0500

    BreadthFirstSearch

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class BreadthFirstSearch<V> implements Search<V> {
+    private Map<Vertex<V>, Vertex<V>> edgeTo = new HashMap<>();
+    private Set<Vertex<V>> marked = new HashSet<>();
+    private Vertex<V> start;
+
+    public BreadthFirstSearch(WeightedGraph<V> graph, Vertex<V> start) {
+        this.start = start;
+        bfs(graph, start);
+    }
+
+    private void bfs(WeightedGraph<V> graph, Vertex<V> root) {
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        queue.add(root);
+        marked.add(root);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> current = queue.poll();
+            for (Vertex<V> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!marked.contains(neighbor)) {
+                    marked.add(neighbor);
+                    edgeTo.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean hasPathTo(Vertex<V> destination) {
+        return marked.contains(destination);
+    }
+
+    @Override
+    public List<Vertex<V>> pathTo(Vertex<V> destination) {
+        if (!hasPathTo(destination)) return null;
+
+        LinkedList<Vertex<V>> path = new LinkedList<>();
+        for (Vertex<V> x = destination; x != start; x = edgeTo.get(x)) {
+            path.addFirst(x);
+        }
+        path.addFirst(start);
+        return path;
+    }
+}
