0860d6bc084bb0276f57ebae4964ea1e798e8b6c
assignment <aliya_sadvokasova01@mail.ru>
2025-05-18 16:02:21 +0500

    Vertex

---
+++ src/Vertex.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjacentVertices() {
+        return adjacentVertices;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(data);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) obj;
+        return Objects.equals(data, vertex.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data);
+    }
+}
